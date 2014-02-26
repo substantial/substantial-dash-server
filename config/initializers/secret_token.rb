dac8eda357d2c869724f245c74c341089be97a27
Mars <mars@substantial.com>
2014-02-25 18:07:23 -0800

    Initialize Rails 4 + RSpec project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SubstantialDashServer::Application.config.secret_key_base = 'd124deac72cb19f9d4ab4dc29a835f567b72fe6e9e4b8f7c79bada038d6f0b4a1d773c1ed61f8c9a7a108e236941a966def23ff3f30e9e30a410fe0928139ec3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SubstantialDashServer::Application.config.session_store :cookie_store, key: '_substantial-dash-server_session'
