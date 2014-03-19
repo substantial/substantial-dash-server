da9574b1364b7400c1c8d25db37b5fe872e398d2
Mars <mars@substantial.com>
2014-03-19 16:02:17 -0700

    Code clean-up

--- lib/subscriber_auth.rb
+++ lib/subscriber_auth.rb
@@ -6,22 +6,19 @@ class SubscriberAuth
       return callback.call(message)
     end
 
-    subscription = message['subscription']
     api_key = message['ext'] && message['ext']['apiKey']
-
     unless api_key
-      Rails.logger.debug("SubscriberAuth halted for #{subscription} without API key")
+      Rails.logger.debug("SubscriberAuth halted without API key")
       message['error'] = 'API key is required'
       callback.call(message)
       return
     end
 
     auth_data = $redis.hgetall("#{KEY_PREFIX}#{api_key}")
-
     if auth_data && !auth_data.empty?
-      Rails.logger.info("SubscriberAuth succeeded for #{subscription}: #{auth_data.inspect}")
+      Rails.logger.info("SubscriberAuth succeeded: #{auth_data.inspect}")
     else
-      Rails.logger.info("SubscriberAuth failed for #{subscription}: #{api_key.inspect}")
+      Rails.logger.info("SubscriberAuth failed for API key: #{api_key.inspect}")
       message['error'] = 'Unauthorized'
       callback.call(message)
       return
